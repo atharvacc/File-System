f0cfea9c0ef24e31d289ff068b2beaf1c83561e3
Chelsy Sesma <cmsesma@ucdavis.edu>
2020-02-24 14:48:28 -0800

    initial work on needed structs

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,7 +7,24 @@
 #include "disk.h"
 #include "fs.h"
 
-/* TODO: Phase 1 */
+typedef struct __attribute__((packed)) superblock {
+	uint8_t signature[8];
+	uint16_t num_total_blocks;
+	uint16_t root_block_index;
+	uint16_t data_bock_index;
+	uint16_t num_data_blocks;
+	uint8_t num_fat_blocks;
+	uint8_t unused_padding[4079];
+} superblock;
+
+typedef struct __attribute__((packed)) root_directory {
+	uint8_t filename[16];
+	uint32_t file_size;
+	uint16_t first_data_block_index;
+	uint8_t unused_padding[10];
+} root_directory;
+
+static uint16_t fat;
 
 int fs_mount(const char *diskname)
 {
@@ -68,4 +85,3 @@ int fs_read(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
 }
-
