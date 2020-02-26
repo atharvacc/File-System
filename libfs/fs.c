39bd50fb1fcae9992f017e2a6f3eda3838837dfa
atharvacc <anchalke@ucdavis.edu>
2020-02-26 13:50:09 -0800

    Added some information to info

--- libfs/fs.c
+++ libfs/fs.c
@@ -87,6 +87,14 @@ int fs_umount(void)
 
 int fs_info(void)
 {
+
+	int data_blk_count = block_disk_count() - (superBlock->num_fat_blocks  + 2);
+	printf("FS Info:\n");
+	printf("Total_blk_count=%d\n", block_disk_count() );
+	printf("fat_blk_count=%d\n", superBlock->num_fat_blocks);
+	printf("rdir_blk=%d\n", superBlock->root_block_index);
+	printf("data_blk=%d\n", superBlock->num_data_blocks);
+	printf("data_blk_count=%d\n", data_blk_count);
 	
 	return 0;
 }
