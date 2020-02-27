d379ba0080069907a0ed6454fe48e37227d3e262
atharvacc <anchalke@ucdavis.edu>
2020-02-26 16:09:27 -0800

    Not sure what rootDir has

--- libfs/fs.c
+++ libfs/fs.c
@@ -101,9 +101,7 @@ int fs_info(void)
 	} // Find fat_free_ratio
 	int root_free_count = 0;
 	for (int i = 0; i <FS_FILE_MAX_COUNT; i++){
-		if(rootDir->filename[0] == "\0"){
-			root_free_count++;
-		}
+		printf("Filename is %d\n", rootDir->filename);
 	}
 	
 	printf("FS Info:\n");
