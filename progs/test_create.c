4c56557f97c4fed5ecbbb004fbdf499bf9e2202a
atharvacc <anchalke@ucdavis.edu>
2020-03-02 22:16:52 -0800

    Added file to mount some stuff

---
+++ progs/test_create.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <fs.h>
+
+
+int main(int argc, char **argv)
+{
+    fs_mount("disk.fs");
+    fs_create("myFile.txt");
+    fs_umount();
+    return 0;
+
+}
