ff6c1c62888676b8dbfcbe1ba672784f2518f568
atharvacc <anchalke@ucdavis.edu>
2020-03-02 22:06:57 -0800

    Testing fs_create()

--- libfs/fs.c
+++ libfs/fs.c
@@ -151,6 +151,7 @@ int fs_info(void)
 
 int fs_create(const char *filename)
 {
+	/*
 	if ( strlen(filename)+1 > FS_FILENAME_LEN ||  num_files+1 > FS_FILE_MAX_COUNT){ //return -1 if string @filename is too long or if the root directory already contains* %FS_FILE_MAX_COUNT files
 		return -1;
 	}
@@ -177,7 +178,7 @@ int fs_create(const char *filename)
 			strcpy( (char*)rootDir[i].filename , filename);
 		} // If empty slot then can create
 	}// Iterate through every available root dir entry to find an empty slot
-
+*/
 	return 0;
 }
 
