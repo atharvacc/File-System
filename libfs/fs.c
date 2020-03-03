216efb46b05702e612270e9a4e9a041bb82ad4f6
atharvacc <anchalke@ucdavis.edu>
2020-03-02 22:13:09 -0800

    Testing fs_create()

--- libfs/fs.c
+++ libfs/fs.c
@@ -132,7 +132,6 @@ int fs_info(void)
 	} // Find fat_free_ratio
 	int root_free_count = 0;
 	for (int i = 0; i <FS_FILE_MAX_COUNT; i++){
-		printf("Here \n");
 		if (rootDir[i].filename[0] == '\0'){
 			root_free_count++;
 		}
