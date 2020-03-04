932dff9f7e9d8733469046844bad31a57942d158
atharvacc <anchalke@ucdavis.edu>
2020-03-04 02:11:01 -0800

    Updated

--- libfs/fs.c
+++ libfs/fs.c
@@ -51,6 +51,10 @@ int num_open_files = 0;
 
 
 
+/* HELPER FUNCTION */
+int get_data_index(int fd, int offset){
+	int offset_idx = file_descriptor[fd].root_dir->first_data_block_index
+}
 int fs_mount(const char *diskname)
 {
 	// Initialize globals
